e96026b7e9c92c8ac47fea771a632afa535114bc
Ondrea Kanwhen <nendae@hotmail.com>
2019-09-23 13:23:14 -0400

    done again!!

--- bash.js
+++ bash.js
@@ -1,12 +1,11 @@
 process.stdout.write('prompt > ');
-const pwd = require('./pwd')
-
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
 
   if (cmd === 'pwd') {
+    const pwd = require('./pwd')
     pwd();
   } else if (cmd === 'ls'){
     const ls = require('./ls')
